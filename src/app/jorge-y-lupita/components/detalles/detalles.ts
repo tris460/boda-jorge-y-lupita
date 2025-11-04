2018e821cb8a8e12b5dea7e9ed75546621710649
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2025-11-03 23:44:57 -0600

    style details

--- src/app/jorge-y-lupita/components/detalles/detalles.ts
+++ src/app/jorge-y-lupita/components/detalles/detalles.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-detalles',
@@ -6,4 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './detalles.html',
   styleUrl: './detalles.scss'
 })
-export class Detalles {}
+export class Detalles implements OnInit, OnDestroy {
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  private interval: any;
+
+  ngOnInit() {
+    this.startCountdown();
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  private startCountdown() {
+    const weddingDate = new Date('2026-05-23T19:30:00').getTime();
+    
+    this.interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = weddingDate - now;
+      
+      if (distance > 0) {
+        this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      } else {
+        this.days = this.hours = this.minutes = this.seconds = 0;
+      }
+    }, 1000);
+  }
+}
