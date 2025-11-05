eebe6f835bb2bd60dc47c9da86b87f1a6fd21f57
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2025-11-04 21:10:08 -0600

    add main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ByeComponent } from './b-y-e/b-y-e';
 import { JorgeYLupitaComponent } from './jorge-y-lupita/jorge-y-lupita';
+import { HomeComponent } from './home/home';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'wedding-b-y-e', component: ByeComponent },
-  { path: 'jorge-y-lupita', component: JorgeYLupitaComponent },
-  { path: '', redirectTo: '/wedding-b-y-e', pathMatch: 'full' }
+  { path: 'jorge-y-lupita', component: JorgeYLupitaComponent }
 ];
